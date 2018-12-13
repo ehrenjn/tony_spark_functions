d2f403b7b061b360e4caac6bd9d275a4512021fa
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2018-12-12 23:15:39 -0500

    added playables

--- wak_funcs.py
+++ wak_funcs.py
@@ -5,11 +5,53 @@ import requests
 import random
 import re
 import discord
+import asyncio
+import json
+
+
+class JSONStore:
+        
+    def __init__(self, file_name):
+        self._file = file_name
+        try:
+            with open(file_name, 'r') as f:
+                self._store = json.loads(f.read())
+        except FileNotFoundError:
+            self._store = dict()
+        
+    def __setitem__(self, key, value):
+        if not type(key) == str:
+            raise Exception('Sorry, JSON can only store string keys')
+        self._store[key] = value
+        self.update()
+            
+    def __getitem__(self, key):
+        return self._store.get(key)
+
+    def update(self):
+        '''updates the contents of the json file
+            called when setting an item via __setitem__
+            useful when storing mutable objects, ie:
+            store['list'].append('item'); store.update()'''
+        with open(self._file, 'w') as f:
+            f.write(json.dumps(self._store))
+
+
+STORAGE_FILE = 'wak_storage.json'
+
+class WakStore(JSONStore):
+    def __init__(self):
+        super().__init__(STORAGE_FILE)
+        if self['playables'] is None: #init playables so I don't have to keep checking if they're None
+            self['playables'] = []
+        
 
 def setup(bot):
 
+    storage = WakStore()
+
     @bot.command(name = "eval")
-    async def execute(ctx, *, cmd):
+    async def execute(ctx, *, cmd): #if cmd arg is keyword only it lets discordpy know to pass in args as one string
         import random #import locally because I don't want eval to have global namespace
         import math
         import re
@@ -28,7 +70,6 @@ def setup(bot):
         else:
             await ctx.send(return_val)
 
-
     @bot.command()
     async def img(ctx, *args):
         query = '+'.join(args) + '&source=lnms&tbm=isch'
@@ -42,3 +83,48 @@ def setup(bot):
             embed.set_image(url = random.choice(imgs))
             await ctx.send(embed = embed)
 
+    @bot.command()
+    async def play(ctx, *, cmd):
+        storage['playables'].append(cmd)
+        storage.update()
+        await bot.change_presence(game = discord.Game(name = cmd))
+        await ctx.send('added playable')
+
+
+    async def play_random_playable():
+        playables = storage['playables']
+        if len(playables) > 0:
+            new_game = discord.Game(name = random.choice(playables))
+            await bot.change_presence(game = new_game)
+        else:
+            await bot.change_presence(game = None)
+
+    @bot.command()
+    async def unplay(ctx, *, cmd):
+        playables = storage['playables']
+        if cmd in playables:
+            playables.remove(cmd)
+            storage.update()
+            if ctx.guild is not None and ctx.guild.me.game.name not in playables: #ctx.guild is None if in DMs
+                await play_random_playable()
+            await ctx.send("removed playable")
+        else:
+            await ctx.send("Couldn't find playable: " + cmd)
+
+    async def background():
+        print('wak background process started')
+        while bot.ws is None: #wait until ws connection is made (there is a short period of time after bot.run is called during which the event loop has started but a discord websocket hasn't been established)
+            await asyncio.sleep(1)
+        while True:
+            await play_random_playable()
+            await asyncio.sleep(int(random.random()*30*60))
+    bot.loop.create_task(background())
+
+
+'''
+from discord.ext import commands
+TOKEN = 
+bot = commands.Bot(command_prefix = '!', case_insensitive = False)
+setup(bot)
+bot.run(TOKEN)
+'''
